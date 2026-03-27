df7c912fb59ff8076527df4a604dff23fc06e7f7
Simhadri Tanush Kumar <tanushkumar2006@gmail.com>
2026-03-27 20:42:04 +0530

    Update MahathiContractorApplication.java

--- backend/src/main/java/com/mahathi/contractor/MahathiContractorApplication.java
+++ backend/src/main/java/com/mahathi/contractor/MahathiContractorApplication.java
@@ -2,11 +2,31 @@ package com.mahathi.contractor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MahathiContractorApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(MahathiContractorApplication.class, args);
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        
+        return new CorsFilter(source);
     }
 }
